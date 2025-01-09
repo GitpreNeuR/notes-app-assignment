8e95aae85faebe633e7736174b8f40ee9b01fd6b
GitpreNeuR <95102789+GitpreNeuR@users.noreply.github.com>
2025-01-09 21:56:06 +0530

    Update SignUp.jsx

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -168,7 +168,7 @@ const SignUp = () => {
       onClick={() => setShowOtp(!showOtp)}
       className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-400 hover:text-gray-600"
     >
-      {showOtp ? <EyeOff size={20} /> : <Eye size={20} />} {/* Toggle icon */}
+      {showOtp ? <Eye size={20} /> : <EyeOff size={20} />} {/* Toggle icon */}
     </button>
             </div>
             <button
